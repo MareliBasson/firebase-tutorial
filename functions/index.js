d3133c3d1d5338e57fbc8c261d87d7d6c463b189
Mareli Basson <ambra.lioness@gmail.com>
2020-04-23 11:04:01 +0200

    add signup functionality

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
 const functions = require("firebase-functions")
 const app = require("express")()
 
+const auth = require("./util/auth")
 const { getAllTodos, postOneTodo, deleteTodo, editTodo } = require("./APIs/todos")
-const { loginUser } = require("./APIs/users")
+const { loginUser, signUpUser, uploadProfilePhoto } = require("./APIs/users")
 
 // Todos
 app.get("/todos", getAllTodos)
@@ -12,6 +13,7 @@ app.put("/todo/:todoId", editTodo)
 
 // Users
 app.post("/login", loginUser)
-
+app.post("/signup", signUpUser)
+app.post("/user/image", auth, uploadProfilePhoto)
 
 exports.api = functions.https.onRequest(app)
